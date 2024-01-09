f82e38325b39d3d7f908a4840a979c61e21d527d
Smile-Dev <86767295+olatf@users.noreply.github.com>
2024-01-09 08:09:12 +0100

    Update settings.py

--- dev_resume/settings.py
+++ dev_resume/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False ,cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://olatf.github.io/resume/",
+]
 
 
 # Application definition
